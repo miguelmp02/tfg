784f703acdb82eae1ca420c40874156aa44ab380
Miguel <miguel.merinoplaza@gmail.com>
2024-06-19 19:21:07 +0200

    cambios

--- src/main.c
+++ src/main.c
@@ -1,22 +1,22 @@
-#include "symbol_table.h"
+#include "alfa.tab.h"  // Asegúrate de que este archivo incluya las definiciones necesarias de Bison y Flex
 
-extern int yyparse(void);
+extern FILE *yyin;    // Asegura que yyin es reconocido como externo si no está en alfa.tab.h
 
 int main(int argc, char **argv) {
     if (argc > 1) {
         FILE *file = fopen(argv[1], "r");
         if (!file) {
             fprintf(stderr, "No se pudo abrir el archivo: %s\n", argv[1]);
-            return EXIT_FAILURE;
+            return 1;
         }
         yyin = file;
     }
 
-    if (yyparse() != 0) {
-        fprintf(stderr, "Se encontraron errores en el compilado\n");
-        return EXIT_FAILURE;
+    yyparse();
+
+    if (yyin != stdin) {
+        fclose(yyin);
     }
 
-    fclose(yyin);
-    return EXIT_SUCCESS;
+    return 0;
 }
